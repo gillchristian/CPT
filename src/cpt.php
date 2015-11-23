84ecfe3cc1af3f1758b38c76de2384309b8f4423
gillchristian <gillchristiang@gmail.com>
2015-11-23 03:35:25 -0300

    Several changes
    Added Taxonomy and cptProvider Classes. Refactored all the code improving OOD, $args defaults and custom hadling, improved inline documentation.

--- src/cpt.php
+++ src/cpt.php
@@ -10,227 +10,256 @@
 
 include_once('inflector.php');
 
-class CPT {
+class Post_Type {
 
 	// cpt properties
-	public $type;
- 	private $plural;
-	private $title;
-	private $plural_title;
+	public $slug;
+ 	public $plural;
+	public $title;
+	public $plural_title;
 
 	// arrays properties for custom post type options
-	private $args = [];
-	private $labels = [];
+	public $args = array();
+	public $labels = array();
 
-	// taxonomies properties
-	private $tax_name;
-	private $tax_plural;
-	private $tax_title;
-	private $tax_plural_title;
+	/**
+	 * Constructor
+	 *
+	 * @param {string: $slug}
+	 */
 
-	// arrays properties for taxonomies options
-	private $tax_args = [];
-	private $tax_labels = [];
+	function __construct($slug){
 
-	//Constructor
- 
-	function __construct($par_name){
-		
-		$this->type = $par_name;
-		$this->plural = Inflector::pluralize($par_name);
-		$this->title = Inflector::titleize($par_name);
+		$this->slug = $slug;
+		$this->plural = Inflector::pluralize($slug);
+		$this->title = Inflector::titleize($slug);
 		$this->plural_title = Inflector::titleize($this->plural);
+		//die($this->plural_title);
 	}
 
-	//Functions
-
-	//Post type register
-	
-	public function add_post_type(){	
-
-		add_action('init', array($this, 'custom_post_register' ));
-		$this->custom_post_register();
-		
+	/**
+	 * Adds the action hook for registering a post type.
+	 */
+	public function register_post_type(){
+		add_action('init', array($this, 'new_post_type' ));
 	}
 
-	public function custom_post_register() {
+	/**
+	 * Generates the $args array and registers the post type
+	 *
+	 * Callback passed to WordPress.
+	 * By default is created not hierarchical, you can do $this->set_args to change it to hierarchical.
+	 */
+	public function new_post_type() {
+		$args = cptProvider::cpt_args($this->slug, $this->plural, $this->title, $this->plural_title);
 
-		$name = $this->type;
-		$plural = $this->plural;
-		$title = $this->title;
-		$plural_title = $this->plural_title;
+		$args['labels'] = array_merge($args['labels'], $this->labels);
+		$args = array_merge($args, $this->args);
 
-	    $labels_aux = array(
-			'name'               => _x( $plural_title, 'post type general name'),
-			'singular_name'      => _x( $title, 'post type singular name'),
-			'menu_name'          => _x( $plural_title, 'admin menu'),
-			'name_admin_bar'     => _x( $title, 'add new on admin bar'),
-			'add_new'            => _x( 'Add New', $name),
-			'add_new_item'       => __( "Add New $title"),
-			'new_item'           => __( "New $title"),
-			'edit_item'          => __( "Edit $title"),
-			'view_item'          => __( "View $title"),
-			'all_items'          => __( "All $plural_title"),
-			'search_items'       => __( "Search $plural_title"),
-			'parent_item_colon'  => __( "Parent $plural_title:"),
-			'not_found'          => __( "No $plural found."),
-			'not_found_in_trash' => __( "No $plural found in Trash.")
+		register_post_type( $this->slug , $args );
+	}
+
+	/**
+	 * Unregisters a Post Type
+	 */
+	public function unregister(){
+		global $wp_post_types;
+		if ( isset( $wp_post_types[ $this->slug ] ) ) unset( $wp_post_types[ $this->slug ] );
+	}
+
+	/**
+	 * Set $args
+	 * @param {$options: array}
+	 */
+	public function set_args ($options) {
+		$this->args = $options;
+	}
+
+	/**
+	 * Set $labels
+	 * @param {$options: array}
+	 */
+	public function set_labels ($options) {
+		$this->labels = $options;
+	}
+
+}
+
+/**
+ * Register taxonomies
+ *
+ */
+
+class Taxonomy extends Post_Type{
+
+	public $hierarchical;
+	public $post_types;
+
+	/**
+	 * Adds the action hook for registering a Taxonomy.
+	 *
+	 * @param {string|array: $post_types} the post types to bind the taxonomy to.
+	 * @param {boolean: $hierarchical = false} determinates whether the taxonomy is hierarchical or not.
+	 */
+	public function register_taxonomy($post_types, $hierarchical = false){
+		$this->post_types = $post_types;
+		$this->hierarchical = $hierarchical;
+		add_action('init', array($this, 'new_taxonomy'));
+	}
+
+	/**
+	 * Generates the $args array and registers the taxonomy
+	 *
+	 * Callback passed to WordPress.
+	 * By default is created not hierarchical, you can change that by passing true to $this->register_taxonomy.
+	 */
+	public function new_taxonomy() {
+
+		$args = $this->hierarchical ?
+							cptProvider::category_args($this->slug, $this->plural, $this->title, $this->plural_title) :
+							cptProvider::tag_args($this->slug, $this->plural, $this->title, $this->plural_title);
+
+		$args['labels'] = array_merge($args['labels'], $this->labels);
+		$args = array_merge($args, $this->args);
+
+    register_taxonomy( $this->slug, $this->post_types, $args );
+	}
+
+}
+
+/**
+ * CPT Provider Class
+ *
+ * Methods return the defaults CPT and Taxonomies $args arrays
+ */
+class cptProvider {
+
+	/**
+	 * Custom Post Type arguments
+	 *
+	 * @param {string: $slug} Slug of the CPT
+	 * @param {string: $plural_slug} Plural slug
+	 * @param {string: $name} Name of the  CPT
+	 * @param {string: $plural_slug} Plural Name
+	 *
+	 * @return {array: $args} default $args with the injected parameters
+	 */
+	static function cpt_args($slug, $plural_slug, $name, $plural_name) {
+		$labels = array(
+			'name'               => _x( $plural_name, 'post type general name', 'your-plugin-textdomain' ),
+			'singular_name'      => _x( $name, 'post type singular name', 'your-plugin-textdomain' ),
+			'menu_name'          => _x( $plural_name, 'admin menu', 'your-plugin-textdomain' ),
+			'name_admin_bar'     => _x( $name, 'add new on admin bar', 'your-plugin-textdomain' ),
+			'add_new'            => _x( 'Add New', $slug, 'your-plugin-textdomain' ),
+			'add_new_item'       => __( 'Add New '.$name, 'your-plugin-textdomain' ),
+			'new_item'           => __( 'New '.$name, 'your-plugin-textdomain' ),
+			'edit_item'          => __( 'Edit '.$name, 'your-plugin-textdomain' ),
+			'view_item'          => __( 'View '.$name, 'your-plugin-textdomain' ),
+			'all_items'          => __( 'All '.$plural_slug, 'your-plugin-textdomain' ),
+			'search_items'       => __( 'Search '.$plural_slug, 'your-plugin-textdomain' ),
+			'parent_item_colon'  => __( 'Parent '.$plural_slug.':', 'your-plugin-textdomain' ),
+			'not_found'          => __( 'No '.$plural_name.' found.', 'your-plugin-textdomain' ),
+			'not_found_in_trash' => __( 'No '.$plural_name.' found in Trash.', 'your-plugin-textdomain' )
 		);
 
-	    $labels = array_merge($labels_aux, $this->labels);
-	    $this->labels = [];
-
-		$args_aux = array(
+		$args = array(
 			'labels'             => $labels,
+			'description'        => __( $name.' post type.', 'your-plugin-textdomain' ),
 			'public'             => true,
 			'publicly_queryable' => true,
 			'show_ui'            => true,
 			'show_in_menu'       => true,
 			'query_var'          => true,
-			'rewrite'            => array( 'slug' => $name ),
+			'rewrite'            => array( 'slug' => $slug ),
 			'capability_type'    => 'post',
 			'has_archive'        => true,
 			'hierarchical'       => false,
 			'menu_position'      => null,
-			'supports'           => array( 'title', 'editor', 'author', 'thumbnail')
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
 		);
 
-	    $args = array_merge($args_aux, $this->args);
-	    $this->args = [];
-
-	    register_post_type( $name , $args );
+		return $args;
 	}
 
-	// Unregister post type
-	
-	public function unregister_post_type(){
-	
-		global $wp_post_types;
-		
-		if ( isset( $wp_post_types[ $this->type ] ) ) unset( $wp_post_types[ $this->type ] );
+	/**
+	 * Custom Hierarchical Taxonomy Arguments
+	 *
+	 * @param {string: $slug} Slug of the Taxonomy
+	 * @param {string: $plural_slug} Plural slug
+	 * @param {string: $name} Name of the  Taxonomy
+	 * @param {string: $plural_slug} Plural Name
+	 *
+	 * @return {array: $args} default $args with the injected parameters
+	 */
+
+	static function category_args($slug, $plural_slug, $name, $plural_name){
+		$labels = array(
+			'name'              => _x( $plural_name, 'taxonomy general name' ),
+			'singular_name'     => _x( $name, 'taxonomy singular name' ),
+			'search_items'      => __( 'Search '.$plural_name ),
+			'all_items'         => __( 'All '.$plural_name ),
+			'parent_item'       => __( 'Parent '.$name ),
+			'parent_item_colon' => __( 'Parent '.$name.':' ),
+			'edit_item'         => __( 'Edit '.$name ),
+			'update_item'       => __( 'Update '.$name ),
+			'add_new_item'      => __( 'Add New '.$name ),
+			'new_item_name'     => __( 'New '.$name.' Name' ),
+			'menu_name'         => __( $name ),
+		);
+
+		$args = array(
+			'hierarchical'      => true,
+			'labels'            => $labels,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'rewrite'           => array( 'slug' => $slug ),
+		);
+
+		return $args;
 	}
 
-	// Changes the $args options of the cpt
-	public function cpt_args_options ($options) {
-		
-		$this->args = [];
-	    $this->args = array_merge($options);
+	/**
+	 * Custom Not Hierarchical Taxonomy Arguments
+	 *
+	 * @param {string: $slug} Slug of the Taxonomy
+	 * @param {string: $plural_slug} Plural slug
+	 * @param {string: $name} Name of the  Taxonomy
+	 * @param {string: $plural_slug} Plural Name
+	 *
+	 * @return {array: $args} default $args with the injected parameters
+	 */
+
+	static function tag_args($slug, $plural_slug, $name, $plural_name){
+		$labels = array(
+			'name'                       => _x( $plural_name, 'taxonomy general name' ),
+			'singular_name'              => _x( $name, 'taxonomy singular name' ),
+			'search_items'               => __( 'Search '.$plural_name ),
+			'popular_items'              => __( 'Popular '.$plural_name ),
+			'all_items'                  => __( 'All '.$plural_name ),
+			'parent_item'                => null,
+			'parent_item_colon'          => null,
+			'edit_item'                  => __( 'Edit '.$name ),
+			'update_item'                => __( 'Update '.$name ),
+			'add_new_item'               => __( 'Add New '.$name ),
+			'new_item_name'              => __( 'New '.$name.' Name' ),
+			'separate_items_with_commas' => __( 'Separate '.$plural_slug.' with commas' ),
+			'add_or_remove_items'        => __( 'Add or remove '.$plural_slug ),
+			'choose_from_most_used'      => __( 'Choose from the most used '.$plural_slug ),
+			'not_found'                  => __( 'No '.$plural_slug.' found.' ),
+			'menu_name'                  => __( $plural_name ),
+		);
+
+		$args = array(
+			'hierarchical'          => false,
+			'labels'                => $labels,
+			'show_ui'               => true,
+			'show_admin_column'     => true,
+			'update_count_callback' => '_update_post_term_count',
+			'query_var'             => true,
+			'rewrite'               => array( 'slug' => $slug ),
+		);
+
+		return $args;
 	}
-
-	// Changes the $labels options of the cpt
-	public function cpt_labels_options ($options) {
-		
-		$this->labels = [];
-	    $this->labels = array_merge($options);
-	}
-
-	// Taxonomy register
-	
-	public function add_taxonomy($par_name, $hierarchical = true){
-
-		$this->tax_name = $par_name;
-		$this->tax_plural = Inflector::pluralize($par_name);
-		$this->tax_title = Inflector::titleize($par_name);
-		$this->tax_plural_title = Inflector::titleize($this->tax_plural);
-		$this->tax_hierarchical = $hierarchical;
-
-		add_action('init', array($this, 'taxonomy_register')); 
-		$this->taxonomy_register();
-		
-	}
-
-	public function taxonomy_register() {
-
-	    $name = $this->tax_name;
-	    $plural = $this->tax_plural;
-	    $title = $this->tax_title;
-	    $plural_title = $this->tax_plural_title;
-	    $post_type = $this->type;
-
-	    if ( $this->tax_hierarchical ) {
-			    
-		    $labels_aux = array(
-		        'name'              => _x( $plural_title, 'taxonomy general name' ),
-		        'singular_name'     => _x( $title, 'taxonomy singular name' ),
-		        'search_items'      => __( 'Search '.$plural_title ),
-		        'all_items'         => __( 'All '.$plural_title ),
-		        'parent_item'       => __( 'Parent '.$title ),
-		        'parent_item_colon' => __( 'Parent '.$title.':' ),
-		        'edit_item'         => __( 'Edit '.$title ),
-		        'update_item'       => __( 'Update '.$title ),
-		        'add_new_item'      => __( 'Add new '.$title ),
-		        'new_item_name'     => __( 'New '.$title.' name'),
-		        'menu_name'         => __( $title ),
-		    );
-
-	    	$labels = array_merge($labels_aux, $this->tax_labels);
-		    $this->tax_labels = [];
-
-		    $args_aux = array(
-		        'hierarchical'      => true,
-		        'labels'            => $labels,
-		        'show_ui'           => true,
-		        'show_admin_column' => true,
-		        'query_var'         => true,
-		        'rewrite'           => array( 'slug' => $name ),
-		    );
-
-	    	$args = array_merge($args_aux, $this->tax_args);
-		    $this->tax_args = [];
-	    } 
-	    else {
-	    		
-			$labels_aux = array(
-				'name'                       => _x( $plural_title, 'taxonomy general name' ),
-				'singular_name'              => _x( $name, 'taxonomy singular name' ),
-				'search_items'               => __( 'Search '.$plural_title ),
-				'popular_items'              => __( 'Popular '.$plural_title ),
-				'all_items'                  => __( 'All '.$plural_title ),
-				'parent_item'                => null,
-				'parent_item_colon'          => null,
-				'edit_item'                  => __( 'Edit '.$name),
-				'update_item'                => __( 'Update '.$name),
-				'add_new_item'               => __( 'Add new '.$name),
-				'new_item_name'              => __( 'New '.$name.' name'),
-				'separate_items_with_commas' => __( 'Separate '.$plural.' with commas' ),
-				'add_or_remove_items'        => __( 'Add or remove '.$plural ),
-				'choose_from_most_used'      => __( 'Choose from the most used '.$plural),
-				'not_found'                  => __( 'No '.$plural.' found' ),
-				'menu_name'                  => __( $plural_title )
-			);
-
-	    	$labels = array_merge($labels_aux, $this->tax_labels);
-		    $this->tax_labels = [];
-
-			$args_aux = array(
-				'hierarchical'          => false,
-				'labels'                => $labels,
-				'show_ui'               => true,
-				'show_admin_column'     => true,
-				'update_count_callback' => '_update_post_term_count',
-				'query_var'             => true,
-				'rewrite'               => array( 'slug' => $name ),
-			);
-			
-	    	$args = array_merge($args_aux, $this->tax_args);
-		    $this->tax_args = [];
-	    }
-
-	    register_taxonomy( $name, array($post_type), $args );
-	}
-
-	// Changes the $args options of the taxonomy
-	public function taxonomy_args_options ($options) {
-		
-		$this->tax_args = [];
-    	$this->tax_args = array_merge($options);
-	}
-
-	// Changes the $labels options of taxonomy
-	public function taxonomy_labels_options ($options) {
-		
-		$this->tax_labels = [];
-    	$this->tax_labels = array_merge($options);
-	}
-
 }
