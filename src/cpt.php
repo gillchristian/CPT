064fbb1807501bc5e78cdb8304a2f2994c785ce7
gillchristian <gillchristiang@gmail.com>
2015-12-23 19:07:22 -0300

    minor syntax changes

--- src/cpt.php
+++ src/cpt.php
@@ -95,7 +95,7 @@ class PostType {
 	 * by calling {PostType object}->setArgs( array('hierarchical' => false,) ).
 	 */
 	public function register(){
-		add_action('init', array($this, 'newPostType' ));
+		add_action( 'init', array($this, 'newPostType') );
 	}
 
 	/**
@@ -142,7 +142,7 @@ class PostType {
 	 *
 	 * @return {array} mixed parameters and labesl for the registration
 	 */
-	public function mergeArgs( $args ){
+	public function mergeArgs($args){
 		$args['labels'] = array_merge($args['labels'], $this->labels);
 		$args = array_merge($args, $this->args);
 
@@ -186,7 +186,7 @@ class Taxonomy extends PostType{
 	public function register($postTypes, $hierarchical = false){
 		$this->postTypes = $postTypes;
 		$this->hierarchical = $hierarchical;
-		add_action('init', array($this, 'newTaxonomy'));
+		add_action( 'init', array($this, 'newTaxonomy') );
 	}
 
 	/**
@@ -220,7 +220,7 @@ class CptProvider {
 	 * @return {string} Actual theme text domain
 	 */
 	static function getTextDomain(){
-		return wp_get_theme()->get('TextDomain ');
+		return wp_get_theme()->get('TextDomain');
 	}
 
 	/**
@@ -240,36 +240,36 @@ class CptProvider {
 		$domain = self::getTextDomain();
 
 		$labels = array(
-			'name'               => sprintf( _x( '%s', 'post type general name', $domain ), $pluralName ),
-			'singular_name'      => sprintf( _x( '%s', 'post type singular name', $domain ), $name ),
-			'menu_name'          => sprintf( _x( '%s', 'admin menu name', $domain ), $pluralName ),
-			'name_admin_bar'     => sprintf( _x( '%s', 'add new on admin bar', $domain ), $name ),
-			'add_new'            => _x( 'Add New', 'add new menu option', $domain ),
-			'add_new_item'       => sprintf( _x( 'Add New %s', 'add new single view', $domain ), $name ),
-			'new_item'           => sprintf( __( 'New %s', $domain ), $name ),
-			'edit_item'          => sprintf( __( 'Edit %s', $domain ), $name ),
-			'view_item'          => sprintf( __( 'View %s', $domain ), $name ),
-			'all_items'          => sprintf( __( 'All %s', $domain ), $plural ),
-			'search_items'       => sprintf( __( 'Search %s', $domain ), $plural ),
-			'parent_item_colon'  => sprintf( __( 'Parent %s:', $domain ), $plural ),
-			'not_found'          => sprintf( __( 'No %s found.', $domain ), $pluralName ),
-			'not_found_in_trash' => sprintf( __( 'No %s found in Trash.', $domain ), $pluralName )
+			'name'               => sprintf( _x('%s', 'post type general name', $domain), $pluralName ),
+			'singular_name'      => sprintf( _x('%s', 'post type singular name', $domain), $name ),
+			'menu_name'          => sprintf( _x('%s', 'admin menu name', $domain), $pluralName ),
+			'name_admin_bar'     => sprintf( _x('%s', 'add new on admin bar', $domain), $name ),
+			'add_new'            => _x('Add New', 'add new menu option', $domain),
+			'add_new_item'       => sprintf( _x('Add New %s', 'add new single view', $domain), $name ),
+			'new_item'           => sprintf( __('New %s', $domain), $name ),
+			'edit_item'          => sprintf( __('Edit %s', $domain), $name ),
+			'view_item'          => sprintf( __('View %s', $domain), $name ),
+			'all_items'          => sprintf( __('All %s', $domain), $plural ),
+			'search_items'       => sprintf( __('Search %s', $domain), $plural ),
+			'parent_item_colon'  => sprintf( __('Parent %s:', $domain), $plural ),
+			'not_found'          => sprintf( __('No %s found.', $domain), $pluralName ),
+			'not_found_in_trash' => sprintf( __('No %s found in Trash.', $domain), $pluralName )
 		);
 
 		$args = array(
 			'labels'             => $labels,
-			'description'        => sprintf( _x( '%s post type.', 'description', $domain ), $name ),
+			'description'        => sprintf( _x('%s post type.', 'description', $domain), $name ),
 			'public'             => true,
 			'publicly_queryable' => true,
 			'show_ui'            => true,
 			'show_in_menu'       => true,
 			'query_var'          => true,
-			'rewrite'            => array( 'slug' => $slug ),
+			'rewrite'            => array('slug' => $slug),
 			'capability_type'    => 'post',
 			'has_archive'        => true,
 			'hierarchical'       => false,
 			'menu_position'      => null,
-			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+			'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments')
 		);
 		return $args;
 	}
@@ -292,21 +292,21 @@ class CptProvider {
 		$domain = self::getTextDomain();
 
 		$labels = array(
-			'name'              			=> sprintf( _x( '%s', 'taxonomy general name', $domain ), $pluralName ),
-			'singular_name'           => sprintf( _x( '%s', 'taxonomy singular name', $domain ), $name ),
-			'search_items'            => sprintf( __( 'Search %s', $domain ) , $pluralName ),
-			'popular_items'           => sprintf( __( 'Popular %s', $domain ), $pluralName ),
-			'all_items'               => sprintf( __( 'All %s', $domain ), $pluralName ),
-			'parent_item'       			=> sprintf( __( 'Parent %s', $domain ), $name ),
-			'parent_item_colon' 			=> sprintf( __( 'Parent %s:', $domain ), $name),
-			'edit_item'               => sprintf( __( 'Edit %s', $domain ), $name ),
-			'update_item'             => sprintf( __( 'Update %s', $domain ), $name ),
-			'add_new_item'            => sprintf( __( 'Add New %s', $domain ), $name ),
-			'new_item_name'           => sprintf( __( 'New %s Name', $domain ), $name ),
-			'add_or_remove_items'     => sprintf( __( 'Add or remove %s', $domain ), $plural ),
-			'choose_from_most_used' 	=> sprintf( __( 'Choose from the most used %s', $domain ), $plural ),
-			'not_found'               => sprintf( __( 'No %s found.', $domain ), $plural ),
-			'menu_name'               => sprintf( _x( '%s', 'menu name', $domain ), $pluralName ),
+			'name'              			=> sprintf( _x('%s', 'taxonomy general name', $domain), $pluralName ),
+			'singular_name'           => sprintf( _x('%s', 'taxonomy singular name', $domain), $name ),
+			'search_items'            => sprintf( __('Search %s', $domain) , $pluralName ),
+			'popular_items'           => sprintf( __('Popular %s', $domain), $pluralName ),
+			'all_items'               => sprintf( __('All %s', $domain), $pluralName ),
+			'parent_item'       			=> sprintf( __('Parent %s', $domain), $name ),
+			'parent_item_colon' 			=> sprintf( __('Parent %s:', $domain), $name),
+			'edit_item'               => sprintf( __('Edit %s', $domain), $name ),
+			'update_item'             => sprintf( __('Update %s', $domain), $name ),
+			'add_new_item'            => sprintf( __('Add New %s', $domain), $name ),
+			'new_item_name'           => sprintf( __('New %s Name', $domain), $name ),
+			'add_or_remove_items'     => sprintf( __('Add or remove %s', $domain), $plural ),
+			'choose_from_most_used' 	=> sprintf( __('Choose from the most used %s', $domain), $plural ),
+			'not_found'               => sprintf( __('No %s found.', $domain), $plural ),
+			'menu_name'               => sprintf( _x('%s', 'menu name', $domain), $pluralName ),
 		);
 
 		$args = array(
@@ -315,7 +315,7 @@ class CptProvider {
 			'show_ui'           => true,
 			'show_admin_column' => true,
 			'query_var'         => true,
-			'rewrite'           => array( 'slug' => $slug ),
+			'rewrite'           => array('slug' => $slug),
 		);
 		return $args;
 	}
@@ -338,22 +338,22 @@ class CptProvider {
 		$domain = self::getTextDomain();
 
 		$labels = array(
-			'name'                       => sprintf( _x( '%s', 'taxonomy general name', $domain ), $pluralName ),
-			'singular_name'              => sprintf( _x( '%s', 'taxonomy singular name', $domain ), $name ),
-			'search_items'               => sprintf( __( 'Search %s', $domain ) , $pluralName ),
-			'popular_items'              => sprintf( __( 'Popular %s', $domain ), $pluralName ),
-			'all_items'                  => sprintf( __( 'All %s', $domain ), $pluralName ),
+			'name'                       => sprintf( _x('%s', 'taxonomy general name', $domain), $pluralName ),
+			'singular_name'              => sprintf( _x('%s', 'taxonomy singular name', $domain), $name ),
+			'search_items'               => sprintf( __('Search %s', $domain) , $pluralName ),
+			'popular_items'              => sprintf( __('Popular %s', $domain), $pluralName ),
+			'all_items'                  => sprintf( __('All %s', $domain), $pluralName ),
 			'parent_item'                => null,
 			'parent_item_colon'          => null,
-			'edit_item'                  => sprintf( __( 'Edit %s', $domain ), $name ),
-			'update_item'                => sprintf( __( 'Update %s', $domain ), $name ),
-			'add_new_item'               => sprintf( __( 'Add New %s', $domain ), $name ),
-			'new_item_name'              => sprintf( __( 'New %s Name', $domain ), $name ),
-			'separate_items_with_commas' => sprintf( __( 'Separate %s with commas', $domain ), $plural ),
-			'add_or_remove_items'        => sprintf( __( 'Add or remove %s', $domain ), $plural ),
-			'choose_from_most_used'      => sprintf( __( 'Choose from the most used %s', $domain ), $plural ),
-			'not_found'                  => sprintf( __( 'No %s found.', $domain ), $plural ),
-			'menu_name'                  => sprintf( _x( '%s', 'menu name', $domain ), $pluralName ),
+			'edit_item'                  => sprintf( __('Edit %s', $domain), $name ),
+			'update_item'                => sprintf( __('Update %s', $domain), $name ),
+			'add_new_item'               => sprintf( __('Add New %s', $domain), $name ),
+			'new_item_name'              => sprintf( __('New %s Name', $domain), $name ),
+			'separate_items_with_commas' => sprintf( __('Separate %s with commas', $domain), $plural ),
+			'add_or_remove_items'        => sprintf( __('Add or remove %s', $domain), $plural ),
+			'choose_from_most_used'      => sprintf( __('Choose from the most used %s', $domain), $plural ),
+			'not_found'                  => sprintf( __('No %s found.', $domain), $plural ),
+			'menu_name'                  => sprintf( _x('%s', 'menu name', $domain), $pluralName ),
 		);
 
 		$args = array(
@@ -363,7 +363,7 @@ class CptProvider {
 			'show_admin_column'     => true,
 			'update_count_callback' => '_update_post_term_count',
 			'query_var'             => true,
-			'rewrite'               => array( 'slug' => $slug ),
+			'rewrite'               => array('slug' => $slug),
 		);
 		return $args;
 	}
